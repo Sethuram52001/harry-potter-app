19c52b2cf6c9352907c2324c2a6e4ea151013b18
Sethuram.S.V <58566745+Sethuram52001@users.noreply.github.com>
2020-08-15 14:03:36 +0530

    Add files via upload

---
+++ build/precache-manifest.dc2eded039b7d969c0a98d3e361a5dd3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6882d65c2e34d951dd969c1fa66259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8fa76b4003184fe86b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1ad3f5527fafad0a709c",
+    "url": "/static/css/main.be25dac4.chunk.css"
+  },
+  {
+    "revision": "6e8fa76b4003184fe86b",
+    "url": "/static/js/2.6422cccb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.6422cccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad3f5527fafad0a709c",
+    "url": "/static/js/main.741597be.chunk.js"
+  },
+  {
+    "revision": "5b93e584858b114ac6d1",
+    "url": "/static/js/runtime-main.5c17c5fa.js"
+  },
+  {
+    "revision": "01514cb67ea930c931e5440832b4a6e4",
+    "url": "/static/media/Sorting_hall.01514cb6.jpg"
+  },
+  {
+    "revision": "da149205339501d037fd1f9d1ed0d7db",
+    "url": "/static/media/harry-potter-characters.da149205.jpg"
+  },
+  {
+    "revision": "c4a704dd5c6bbe46ecb1e421757733e7",
+    "url": "/static/media/hogwarts.c4a704dd.jpg"
+  },
+  {
+    "revision": "2b6df549d0998cfe96d292689f1c8e0c",
+    "url": "/static/media/hp3.2b6df549.jpg"
+  },
+  {
+    "revision": "669136b812686964370823fe0ec57366",
+    "url": "/static/media/hp5.669136b8.jpg"
+  },
+  {
+    "revision": "4b5f2c437da2dbacba41df499d128397",
+    "url": "/static/media/hp6.4b5f2c43.jpg"
+  },
+  {
+    "revision": "894c13f504ca31769f5b78d41c52bdf4",
+    "url": "/static/media/original.894c13f5.jpg"
+  },
+  {
+    "revision": "f7991065085cec7b80b1b0e26172c959",
+    "url": "/static/media/ravenclaw_img.f7991065.jpg"
+  },
+  {
+    "revision": "9d9af0f0517dfd2b3d92b1977e0b1f8a",
+    "url": "/static/media/sorting-hat-1.9d9af0f0.jpg"
+  },
+  {
+    "revision": "6c07c6d24cae1cc2f93bfe8ba8470fcc",
+    "url": "/static/media/the-wizarding-world-of-harry-potter.6c07c6d2.png"
+  }
+]);
